f43b59f35bd31fc2e1825531e6642d56a679032b
aalokaswal <aalok.aswal@gmail.com>
2016-06-21 00:30:42 +0530

    Update DisplayWebpostsVM.cs

--- WebPosts/WebPosts/ViewModel/DisplayWebpostsVM.cs
+++ WebPosts/WebPosts/ViewModel/DisplayWebpostsVM.cs
@@ -1,32 +1,44 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using WebPosts.Base;
+using WebPosts.WebAccess;
 using WebPosts.BusinnesLogic;
+using WebPosts.Helper;
 using WebPosts.Model;
 
 namespace WebPosts.ViewModel
 {
-    public class DisplayWebpostsVM : ViewModelBase
+    public class WebpostsViewModel : ViewModelBase
     {
-        WebPostBL webPostBlObj;
-        public DisplayWebpostsVM()
+        IWebPostBL objWebPostBl;
+        public WebpostsViewModel()
         {
-            webPostBlObj = new WebPostBL();
+            objWebPostBl =  new WebPostBL();
+            IntializeVariables();
+        }
+
+        public WebpostsViewModel(IWebPostBL objectWebPstBl)
+        {
+            objWebPostBl = objectWebPstBl;
+            IntializeVariables();
+        }
+
+        private void IntializeVariables()
+        {
             LoadWebPosts();
             SelectedPostCommand = new RelayCommand<object>(DisPlayWebPostContent);
             SaveContent = new RelayCommand<Object>(SaveWebPostContent);
+            CopyContent = new RelayCommand<object>(CopyWebPostContent);
         }
 
         public ICommand SaveContent { get; private set; }
         public ICommand SelectedPostCommand { get; private set; }
+        public ICommand CopyContent { get; private set; }
 
         private List<WebPost> webPostList;
         public List<WebPost> WebPostList 
@@ -38,9 +50,7 @@ namespace WebPosts.ViewModel
         private WebPost selectedWebPost;
         public WebPost SelectedWebPost
         {
-            get {
-                return selectedWebPost;
-            }
+            get {return selectedWebPost;}
             set 
             {
                 selectedWebPost = value;
@@ -51,27 +61,15 @@ namespace WebPosts.ViewModel
         private string webPostTitle;
         public string WebPostTitle
         {
-            get 
-            { 
-                return webPostTitle;
-            }
-            set
-            {
-                webPostTitle = value;
-            }
+            get { return webPostTitle;}
+            set { webPostTitle = value;}
         }
 
-        private string webPostContent;
-        public string WebPostContent
+        private string webContent;
+        public string WebContent
         {
-            get
-            {
-                return webPostContent;
-            }
-            set
-            {
-                webPostContent = value;
-            }
+            get { return webContent;}
+            set {webContent = value;}
         }
 
         private string userId;
@@ -127,9 +125,35 @@ namespace WebPosts.ViewModel
             }
         }
 
-        private async void  LoadWebPosts()
+        private string webPostContent;
+        public string WebPostContent
         {
-            webPostList = await webPostBlObj.GetAllWebPost();
+            get
+            {
+                return webPostContent;
+            }
+            set
+            {
+                webPostContent = value;
+            }
+        }
+
+        private string webPostComment;
+        public string WebPostComment
+        {
+            get
+            {
+                return webPostComment;
+            }
+            set
+            {
+                webPostComment = value;
+            }
+        }
+
+        public async void  LoadWebPosts()
+        {
+            webPostList = await objWebPostBl.GetAllWebPost();
         }
 
         public async void DisPlayWebPostContent(object value)
@@ -139,16 +163,31 @@ namespace WebPosts.ViewModel
                 var webPost = (WebPost)value;
                 if (webPost != null)
                 {
-                    selectedWebPost= webPost;
+                    selectedWebPost = webPost;
                     OnPropertyChanged("SelectedWebPost");
                     webPostTitle = webPost.title;
                     OnPropertyChanged("WebPostTitle");
-                    var content = await webPostBlObj.GetWebPostContent(webPost.id.ToString());
-                    webPostContent = content;
-                    OnPropertyChanged("WebPostContent");
 
-                    SetUserInfo(webPost.userId.ToString());
+                    webPostContent = await objWebPostBl.GetWebPostContentById(webPost.id.ToString());
+                    webPostComment = await objWebPostBl.GetWebPostCommentContent(webPost.id.ToString());
+
+                    WebContent = WebPostContent + webPostComment;
+                    OnPropertyChanged("WebContent");
                 }
+
+                var user = await objWebPostBl.GetUserById(webPost.userId.ToString());
+                if (user != null)
+                {
+                    userId = user.id.ToString();
+                    OnPropertyChanged("UserId");
+                    webUser = user.username;
+                    OnPropertyChanged("WebUser");
+                    userName = user.name;
+                    OnPropertyChanged("UserName");
+                    userEmail = user.email;
+                    OnPropertyChanged("UserEmail");
+                }
+
             }
             catch(Exception ex)
             {
@@ -157,40 +196,12 @@ namespace WebPosts.ViewModel
             
         }
 
-        private async void SetUserInfo(string userID) 
+        private void CopyWebPostContent(object value)
         {
-            var user = await webPostBlObj.GetUserById(userID); 
-
-            if(user != null)
-            {
-                userId = userID; 
-                OnPropertyChanged("UserId");
-                webUser = user.username;
-                OnPropertyChanged("WebUser");
-                userName = user.name;
-                OnPropertyChanged("UserName");
-                userEmail = user.email;
-                OnPropertyChanged("UserEmail");
-            }
-
+            Clipboard.SetText((string)value); 
         }
 
-        public void TestCode()
-        {
-            string name = "AlokAswal";
-        }
-
-        public void tresdf()
-        {
-            string name = "AlokAswal";
-        }
-        
-        public void yughjtresdf()
-        {
-            string name = "AlokAswal";
-        }
-
-        private void SaveWebPostContent(object value)
+        private async void SaveWebPostContent(object value)
         {
             if (WebPostContent == null || WebPostContent == string.Empty)
                 return;
@@ -216,7 +227,25 @@ namespace WebPosts.ViewModel
                 if ((myStream = saveFileDialog1.OpenFile()) != null)
                 {
                     var sw = new StreamWriter(myStream, uniEncoding);
-                    sw.Write(WebPostContent);
+                    if(fileExtension == "html")
+                    {
+                        var webPost = await objWebPostBl.GetWebPost(SelectedWebPost.id.ToString());
+                        var webPostComments = await objWebPostBl.GetWebPostComments(SelectedWebPost.id.ToString());
+                        var webPostList = new List<WebPost>();
+                        webPostList.Add(webPost);
+                        var webPostHtml = Conversion.GetHtml((IEnumerable<WebPost>)webPostList,"WebPost", x => x.id, x => x.title, x => x.body);
+                        var webPostCommentsHtml = Conversion.GetHtml((IEnumerable<WebPostComment>)webPostComments,"WebPostContent", x => x.postId, x => x.id, x => x.name, x => x.email, x => x.body);
+
+                        var htmlStrbuilder = new StringBuilder();
+                        htmlStrbuilder.Append(webPostHtml);
+                        htmlStrbuilder.Append("  ").Append(webPostCommentsHtml);
+
+                        sw.Write(htmlStrbuilder.ToString());
+                    }
+                    else
+                    {
+                        sw.Write(WebContent);
+                    }
                     sw.Flush();
                     myStream.Seek(0, SeekOrigin.Begin);
                     myStream.Close();
